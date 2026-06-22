d98aea9dd3757b01a448bec22bed04ba1cb9e4ae
tntequiv <131315859+tntequiv@users.noreply.github.com>
2026-06-22 12:57:31 -0400

    Add emotion detection functionality
    Implement emotion detection using Watson NLP service.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,1 +1,30 @@
+import json
+import requests
 
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, json=input_json, headers=headers)
+
+    formatted_response = json.loads(response.text)
+    emotion = formatted_response["emotionPredictions"][0]["emotion"]
+
+    anger_score = emotion["anger"]
+    disgust_score = emotion["disgust"]
+    fear_score = emotion["fear"]
+    joy_score = emotion["joy"]
+    sadness_score = emotion["sadness"]
+
+    dominant_emotion = max(emotion, key=emotion.get)
+
+    return {
+        "anger": anger_score,
+        "disgust": disgust_score,
+        "fear": fear_score,
+        "joy": joy_score,
+        "sadness": sadness_score,
+        "dominant_emotion": dominant_emotion,
+    }
